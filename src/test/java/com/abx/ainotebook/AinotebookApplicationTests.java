030c1cef77eecf59fa83f3e7dd1fb4192c664d65
will_bit <williamc.c999@gmail.com>
2024-01-07 16:46:46 -0500

    reformat

--- src/test/java/com/abx/ainotebook/AinotebookApplicationTests.java
+++ src/test/java/com/abx/ainotebook/AinotebookApplicationTests.java
@@ -1,13 +1,6 @@
 package com.abx.ainotebook;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AinotebookApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+class AinotebookApplicationTests {}
