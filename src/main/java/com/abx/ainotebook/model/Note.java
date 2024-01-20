7ca2787a1409d77b5a0ef3c1b1d8d7115ec42d66
yx-xyc <yx2021@nyu.edu>
2024-01-20 16:05:13 -0500

    modify Note.java

--- src/main/java/com/abx/ainotebook/model/Note.java
+++ src/main/java/com/abx/ainotebook/model/Note.java
@@ -1,12 +1,13 @@
 package com.abx.ainotebook.model;
 
+import java.util.UUID;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "notes")
 public class Note {
     @Id
-    private String id;
+    private UUID id;
 
     private String title;
     private String content;
