2f9866dc197f6b76448d05ca84ae1ef89ace3cd4
Ethan Xu <xuqibao0816@gmail.com>
2024-03-11 17:25:19 +0800

    added check for duplicate users; /authenticate throws null pointer exception

--- src/main/java/com/abx/ainotebook/controller/AuthenticationController.java
+++ src/main/java/com/abx/ainotebook/controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.abx.ainotebook.model.AuthenticationRequest;
 import com.abx.ainotebook.model.AuthenticationResponse;
 import com.abx.ainotebook.model.RegisterRequest;
 import com.abx.ainotebook.service.AuthenticationService;
+import java.util.Objects;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,11 +24,31 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authenticationService.register(request));
+        if (request == null
+                || Objects.equals(request.getEmail(), "")
+                || Objects.equals(request.getPassword(), "")
+                || Objects.equals(request.getFirstname(), "")
+                || Objects.equals(request.getLastname(), "")) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        AuthenticationResponse response = authenticationService.register(request);
+        if (response.getToken() == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+        if (request == null || Objects.equals(request.getEmail(), "") || Objects.equals(request.getPassword(), "")) {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        AuthenticationResponse response = authenticationService.authenticate(request);
+        if (response.getToken() == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+        return ResponseEntity.ok(response);
     }
 }
