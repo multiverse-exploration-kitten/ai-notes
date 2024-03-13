09c0a97e3cc0427064fa46c857fd95453ab1e9f4
Ethan Xu <xuqibao0816@gmail.com>
2024-03-13 23:57:32 +0800

    disabled filters in test

--- src/test/java/com/abx/ainotebook/controller/TrackingControllerTest.java
+++ src/test/java/com/abx/ainotebook/controller/TrackingControllerTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -24,6 +25,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @WebMvcTest(TrackingController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class TrackingControllerTest {
     @Autowired
     private MockMvc mockMvc;
