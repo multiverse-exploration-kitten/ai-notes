5e5c4073571e56905bfa4eb4a4bb9dc69402cb5b
yx-xyc <yx2021@nyu.edu>
2024-03-21 13:11:57 -0400

    merge main

--- src/main/resources/db.migration/V1__initial_schema.sql
+++ src/main/resources/db.migration/V1__initial_schema.sql
@@ -3,24 +3,16 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 
 CREATE TABLE IF NOT EXISTS ai_notes.users (
-<<<<<<< HEAD
-                                              user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-=======
     user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
->>>>>>> af01210 (finished authentication service)
     first_name VARCHAR(255),
     last_name VARCHAR(255),
     email VARCHAR(255),
     password VARCHAR(255),
     role VARCHAR(255)
-<<<<<<< HEAD
     );
-=======
-);
->>>>>>> af01210 (finished authentication service)
 
 CREATE TABLE IF NOT EXISTS ai_notes.notebook (
-                                                 notebook_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    notebook_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID,
     title VARCHAR(255),
     category VARCHAR(255),
@@ -33,3 +25,4 @@ CREATE INDEX "user_id_category_index" ON ai_notes.notebook("user_id","category")
 
 INSERT INTO ai_notes.notebook (notebook_id, user_id, title, category)
 VALUES ('123e4567-e89b-12d3-a456-426655440000', '123e4567-e89b-12d3-a456-426614174000', 'Introduction to AI', 'Education');
+
