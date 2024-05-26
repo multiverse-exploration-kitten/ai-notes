0a14c8cdb881e57e2b549b048f3a483c0f54b2fb
yx-xyc <yx2021@nyu.edu>
2024-05-25 20:10:17 -0400

    try heroku

--- src/main/java/com/abx/ainotebook/AinotebookApplication.java
+++ src/main/java/com/abx/ainotebook/AinotebookApplication.java
@@ -2,11 +2,17 @@ package com.abx.ainotebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AinotebookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AinotebookApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "AI Notebook Service is running!";
     }
 }
