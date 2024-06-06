2f63d1c312169c6ed2242c79e4de40338a8bf9aa
yx-xyc <yx2021@nyu.edu>
2024-06-06 18:49:41 -0400

    update securty config

--- src/main/java/com/abx/ainotebook/configuration/SecurityConfig.java
+++ src/main/java/com/abx/ainotebook/configuration/SecurityConfig.java
@@ -36,11 +36,10 @@ public class SecurityConfig {
                     authorizeHttpRequests
                             .requestMatchers("/api/v1/auth/register", "/api/v1/auth/authenticate")
                             .permitAll();
-                    if (Arrays.asList(env.getActiveProfiles()).contains("dev")
-                            || Arrays.asList(env.getActiveProfiles()).contains("test")) {
-                        authorizeHttpRequests.anyRequest().permitAll();
-                    } else {
+                    if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
                         authorizeHttpRequests.anyRequest().authenticated();
+                    } else {
+                        authorizeHttpRequests.anyRequest().permitAll();
                     }
                 })
                 .sessionManagement(
